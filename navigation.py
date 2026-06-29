b251144c14ac1eec9ba1c0d4378a12bccf485840
sygwak <sygwak@branvisoft.com>
2026-06-29 17:03:03 +0900

    fix : ui 편집 및 네비게이션 제거

--- navigation.py
+++ navigation.py
@@ -1,52 +1,33 @@
-
 NAVIGATION = [
     {
         "title": "음악",
-        "icon": "🎵",
+        "icon": "♫",
         "items": [
             {
                 "name": "MP3 File",
-                "icon": "🚩",
-                "url": "/music/playlist"
-            }, {
+                "icon": "♫",
+                "url": "/music/playlist",
+            },
+            {
                 "name": "Clip 생성",
-                "icon": "✂️",
-                "url": "/music/clips"
-            }, {
+                "icon": "✂",
+                "url": "/music/clips",
+            },
+            {
                 "name": "Lick 연습",
-                "icon": "🎹",
-                "url": "/music/licks"
-            }, {
-                "name": "악보 파일 렌더",
-                "icon": "🎼",
-                "url": "/music/render"
-            }, {
+                "icon": "♪",
+                "url": "/music/licks",
+            },
+            {
+                "name": "악보 파일 폴더",
+                "icon": "◫",
+                "url": "/music/render",
+            },
+            {
                 "name": "연습일지",
-                "icon": "📈",
-                "url": "/music/daily"
-            }
-        ]
+                "icon": "◷",
+                "url": "/music/daily",
+            },
+        ],
     },
-    {
-        "title": "가계",
-        "icon": "📑",
-        "items": [
-            {
-                "name": "가계부",
-                "icon": "📑",
-                "url": "/account"
-            }
-        ]
-    },
-    {
-        "title": "개발",
-        "icon": "💻",
-        "items": [
-            {
-                "name": "개발 도구",
-                "icon": "💻",
-                "url": "/dev"
-            }
-        ]
-    }
 ]
