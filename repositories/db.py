beb545ad1d60f40f70943f0636dd61c72a1a9133
sygwak <sygwak@branvisoft.com>
2026-07-02 12:49:56 +0900

    훅실행용 커밋

--- repositories/db.py
+++ repositories/db.py
@@ -15,7 +15,7 @@ def _build_dsn():
     host = os.getenv("PGHOST", "localhost")
     port = os.getenv("PGPORT", "5432")
     dbname = os.getenv("PGDATABASE", "postgres")
-    user = os.getenv("PGUSER", "postgres")
+    user = os.getenv("PGUSER", "postgress")
     password = os.getenv("PGPASSWORD")
 
     auth = user
