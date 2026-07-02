a64df2a9c582a32859f77ade90414db0a3a355cb
sygwak <sygwak@branvisoft.com>
2026-07-02 14:35:38 +0900

    fix: 한글 깨짐 문제 해결 2차

--- navigation.py
+++ navigation.py
@@ -1,33 +1,30 @@
+
 NAVIGATION = [
     {
         "title": "음악",
-        "icon": "♫",
+        "icon": "🎵",
         "items": [
             {
                 "name": "MP3 File",
-                "icon": "♫",
-                "url": "/music/playlist",
-            },
-            {
+                "icon": "🚩",
+                "url": "/music/playlist"
+            }, {
                 "name": "Clip 생성",
-                "icon": "✂",
-                "url": "/music/clips",
-            },
-            {
+                "icon": "✂️",
+                "url": "/music/clips"
+            }, {
                 "name": "Lick 연습",
-                "icon": "♪",
-                "url": "/music/licks",
-            },
-            {
-                "name": "악보 파일 폴더",
-                "icon": "◫",
-                "url": "/music/render",
-            },
-            {
+                "icon": "🎹",
+                "url": "/music/licks"
+            }, {
+                "name": "악보 파일 렌더",
+                "icon": "🎼",
+                "url": "/music/render"
+            }, {
                 "name": "연습일지",
-                "icon": "◷",
-                "url": "/music/daily",
-            },
-        ],
-    },
+                "icon": "📈",
+                "url": "/music/daily"
+            }
+        ]
+    }
 ]
